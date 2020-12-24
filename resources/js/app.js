14ed94ee6f92786d4ffc87aa4686d105fdb7a024
Sajjad Aljileezi <30444995+SajjadAljileezi@users.noreply.github.com>
2020-12-24 14:33:46 -0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,34 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', component: require('./components/HomeComponent.vue').default},
+
+]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history', // short for `routes: routes`
+})
+
+
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    router,
+});
